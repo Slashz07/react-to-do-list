2c4a94a89c93ad71991e3e626840460768cb9244
Slashz07 <sumitforsaken76248@gmail.com>
2024-05-31 11:47:39 +0530

    todo components linked

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import { useEffect, useState } from 'react'
 import { TodoProvider } from './contexts/ToDos.js'
 import './App.css'
-
+import { Todoitems,TodoForm } from './components/index.js'
 function App() {
   const [todos, setTodos] = useState([])
-  
-  useEffect((todos) => {
+
+   useEffect(() => {
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
+
+  useEffect(() => {
     const todoList = JSON.parse(localStorage.getItem("todos"))
     if (todoList && todoList.length != 0) {
       setTodos(todoList)
     }
   },[])
-  useEffect((todos) => {
-    localStorage.setItem("todos",JSON.stringify(todos))
-  },[todos])
-
+ 
   const addTodo = (todo) => {
     setTodos((arr) => [{
       id:arr.length+1,
       task: todo ,
-      complete:false,
+      completed:false,
     },...arr])
   }
   const updateTodo = (id, todo) => {
@@ -31,7 +32,7 @@ function App() {
   const delTodo = (id) => {
     setTodos((prevArr)=>prevArr.confirm((todoObj)=>todoObj.id!=id))
   }
-  const toggleComplete = (id) => {
+  const togglecomplete= (id) => {
     setTodos((prevArr) => prevArr.map((todoObj) => todoObj.id == id ? {
       ...todoObj,
       completed:!todoObj.completed,//here spread operator breaks the object elements and then the "completed" key's value is overwritten
@@ -39,15 +40,19 @@ function App() {
   }
 
   return (
-    <TodoProvider value={{todos,addTodo,updateTodo,delTodo,toggleComplete}}>
+    <TodoProvider value={{todos,addTodo,updateTodo,delTodo,togglecomplete}}>
    <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */} 
+                    <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
-                        {/*Loop and Add TodoItem here */}
+            {todos.map((todoObj) => (
+              <div key={todoObj.id} className='w-full'>
+              <Todoitems todo={todoObj} />
+              </div>
+            ))}
                     </div>
                 </div>
             </div>
